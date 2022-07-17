992539952ed982d58eb4e9ced9deb67083a67d0c
mattcooperdev <coopandmgc@gmail.com>
2022-07-17 16:03:20 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LMYdaEkwgvj714Mcav5eoIM4ojIIGmaghg8wG4jokmdrt4oxyeVLYZ66BqAAPGeNlibxypE3CQYVfIBgZtTSLhe00Vz8GjYjs',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
